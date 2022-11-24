40b703b08854e121410a798dc2678f5910876b9c
MaNaN-3 <Manan.Patel@iiitb.ac.in>
2022-11-25 02:10:34 +0530

    Jenkins file

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import unittest
 
-from prog1 import summation
+from prog1 import vowel
 
 class TestSum(unittest.TestCase):
     def test1(self):
@@ -9,35 +9,35 @@ class TestSum(unittest.TestCase):
         Test case to add two numbers
         """
         s="arr"
-        result = summation(s)
+        result = vowel(s)
         self.assertEqual(result, 1)
     def test2(self):
         """
         Test case to add two numbers
         """
         s="aiu"
-        result = summation(s)
+        result = vowel(s)
         self.assertEqual(result, 3)
     def test3(self):
         """
         Test case to add two numbers
         """
         s="iuq"
-        result = summation(s)
+        result = vowel(s)
         self.assertEqual(result, 2)
     def test4(self):
         """
         Test case to add two numbers
         """
         s="Arr"
-        result = summation(s)
+        result = vowel(s)
         self.assertEqual(result, 1)
     def test5(self):
         """
         Test case to add two numbers
         """
         s="AIr"
-        result = summation(s)
+        result = vowel(s)
         self.assertEqual(result, 2)
 
 if __name__ == '__main__':
