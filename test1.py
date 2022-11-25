e431fcfd9a84dc65a69ea6e07ad1adb26892d85e
MaNaN-3 <Manan.Patel@iiitb.ac.in>
2022-11-25 13:50:07 +0530

    new stage passed

--- test1.py
+++ test1.py
@@ -25,20 +25,6 @@ class TestSum(unittest.TestCase):
         s="iuq"
         result = vowel(s)
         self.assertEqual(result, 2)
-    def test4(self):
-        """
-        Test case to add two numbers
-        """
-        s="Arr"
-        result = vowel(s)
-        self.assertEqual(result, 1)
-    def test5(self):
-        """
-        Test case to add two numbers
-        """
-        s="AIr"
-        result = vowel(s)
-        self.assertEqual(result, 2)
 
 if __name__ == '__main__':
     unittest.main()
